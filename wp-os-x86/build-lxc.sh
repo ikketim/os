a5e78bec0aff3fc493ba2ad3384295d04f97f346
TrelosLeras <115097582+TrelosLeras@users.noreply.github.com>
2026-05-04 17:29:43 +0300

    Add GITHUB_REPO variable substitution in script

--- wp-os-x86/build-lxc.sh
+++ wp-os-x86/build-lxc.sh
@@ -126,6 +126,7 @@ inject_and_run() {
     -e "s|@@BOTS_DIR@@|${BOTS_DIR}|g" \
     -e "s|@@WEBSERVER_DIR@@|${WEBSERVER_DIR}|g" \
     -e "s|@@WEBSERVER_PORT@@|${WEBSERVER_PORT}|g" \
+    -e "s|@@GITHUB_REPO@@|${GITHUB_REPO}|g" \
     "${SCRIPT_DIR}/rootfs-overlay/usr/local/bin/wp-os-provision.sh" \
     > "$PROVISION_TMP"
 
