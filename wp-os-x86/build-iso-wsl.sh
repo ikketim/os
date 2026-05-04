178b1671eff4fb1dbae692bb093642be66ef59d4
TrelosLeras <115097582+TrelosLeras@users.noreply.github.com>
2026-05-04 17:28:19 +0300

    Add substitution for GITHUB_REPO in provisioning script

--- wp-os-x86/build-iso-wsl.sh
+++ wp-os-x86/build-iso-wsl.sh
@@ -107,6 +107,7 @@ inject_autoinstall() {
     -e "s|@@BOTS_DIR@@|${BOTS_DIR}|g" \
     -e "s|@@WEBSERVER_DIR@@|${WEBSERVER_DIR}|g" \
     -e "s|@@WEBSERVER_PORT@@|${WEBSERVER_PORT}|g" \
+    -e "s|@@GITHUB_REPO@@|${GITHUB_REPO}|g" \
     "${SCRIPT_DIR}/rootfs-overlay/usr/local/bin/wp-os-provision.sh" \
     > "${PAYLOAD_DIR}/wp-os-provision.sh"
   chmod +x "${PAYLOAD_DIR}/wp-os-provision.sh"
