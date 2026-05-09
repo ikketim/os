8114ae69ed1699872c8a5301d4ea08e21d909424
TrelosLeras <115097582+TrelosLeras@users.noreply.github.com>
2026-05-09 18:03:14 +0300

    Add substitution for GITHUB_REPO in build-lxc.sh

--- wp-os-x86/build-lxc.sh
+++ wp-os-x86/build-lxc.sh
@@ -127,6 +127,7 @@ inject_and_run() {
     -e "s|@@WEBSERVER_DIR@@|${WEBSERVER_DIR}|g" \
     -e "s|@@WEBSERVER_PORT@@|${WEBSERVER_PORT}|g" \
     -e "s|@@REPO_BASE@@|${REPO_BASE}|g" \
+    -e "s|@@GITHUB_REPO@@|${GITHUB_REPO}|g" \
     "${SCRIPT_DIR}/rootfs-overlay/usr/local/bin/wp-os-provision.sh" \
     > "$PROVISION_TMP"
 
